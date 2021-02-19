869897dd2e9822302c9c1c5eef2735bc45c38189
Dan Nguyen <nghuudan@gmail.com>
2021-02-19 08:27:23 -0500

    Initial commit

---
+++ bin/graphql-cdk.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { GraphqlCdkStack } from '../lib/graphql-cdk-stack';
+
+const app = new cdk.App();
+new GraphqlCdkStack(app, 'GraphqlCdkStack');
