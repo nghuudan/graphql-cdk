5284e760cee4a123903f022090809c7b34941340
Dan Nguyen <nghuudan@gmail.com>
2021-04-02 21:08:48 -0400

    Add memo queries and mutations

--- lib/graphql-cdk-stack.ts
+++ lib/graphql-cdk-stack.ts
@@ -1,9 +1,71 @@
 import * as cdk from '@aws-cdk/core';
+import * as appsync from '@aws-cdk/aws-appsync';
+import * as db from '@aws-cdk/aws-dynamodb';
+import { join } from 'path';
 
 export class GraphqlCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const api = new appsync.GraphqlApi(this, 'Api', {
+      name: 'Memo',
+      schema: appsync.Schema.fromAsset(join(__dirname, 'schema.graphql')),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: appsync.AuthorizationType.API_KEY,
+        },
+      },
+      xrayEnabled: true,
+    });
+
+    const memoTable = new db.Table(this, 'MemoTable', {
+      partitionKey: {
+        name: 'id',
+        type: db.AttributeType.STRING,
+      },
+    });
+
+    const memoDS = api.addDynamoDbDataSource('memoDataSource', memoTable);
+
+    memoDS.createResolver({
+      typeName: 'Query',
+      fieldName: 'allMemos',
+      requestMappingTemplate: appsync.MappingTemplate.dynamoDbScanTable(),
+      responseMappingTemplate: appsync.MappingTemplate.dynamoDbResultList(),
+    });
+
+    memoDS.createResolver({
+      typeName: 'Query',
+      fieldName: 'memo',
+      requestMappingTemplate: appsync.MappingTemplate.dynamoDbGetItem('id', 'memoId'),
+      responseMappingTemplate: appsync.MappingTemplate.dynamoDbResultItem()
+    });
+
+    memoDS.createResolver({
+      typeName: 'Mutation',
+      fieldName: 'addMemo',
+      requestMappingTemplate: appsync.MappingTemplate.dynamoDbPutItem(
+        appsync.PrimaryKey.partition('id').auto(),
+        appsync.Values.projecting('input'),
+      ),
+      responseMappingTemplate: appsync.MappingTemplate.dynamoDbResultItem(),
+    });
+
+    memoDS.createResolver({
+      typeName: 'Mutation',
+      fieldName: 'deleteMemo',
+      requestMappingTemplate: appsync.MappingTemplate.dynamoDbDeleteItem('id', 'memoId'),
+      responseMappingTemplate: appsync.MappingTemplate.dynamoDbResultItem()
+    });
+
+    memoDS.createResolver({
+      typeName: 'Mutation',
+      fieldName: 'updateMemo',
+      requestMappingTemplate: appsync.MappingTemplate.dynamoDbPutItem(
+        appsync.PrimaryKey.partition('id').is('memoId'),
+        appsync.Values.projecting('input'),
+      ),
+      responseMappingTemplate: appsync.MappingTemplate.dynamoDbResultItem()
+    });
   }
 }
